77c6913c74dbdcc1c18d0af8de4c95ac57b38025
Gordon Yu <gyunit@gmail.com>
2019-08-25 23:23:15 -0700

    Elastic Beanstalk, you can do it!

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const yellReviews = require('./database/index.js');
 const app = express();
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8081; //why does it keep going to 9002?
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
